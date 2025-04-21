5021c69c2bab47ab49c507f4fc3f00935f64d2d4
itallodemesquita <itallodemesquita@outlook.com.br>
2025-04-21 15:05:05 -0300

    fix: incrementando estruturas lógicas

--- demo_interruptor/demo_interruptor.c
+++ demo_interruptor/demo_interruptor.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-// Definição dos pinos do LED e Botão na BitDogLab
-#define pino_vermelho 11  // Pino do LED Vermelho
-#define botao_A 5  // Pino do Botão A
 
 int main() {
     // Inicializa o padrão de entrada/saída
+
+    // Definição dos pinos do LED e Botão na BitDogLab
+    const uint pino_vermelho = 13;
+    const uint botao_A = 5;
+
     stdio_init_all();
     
     // Configura o pino do LED como saída
     gpio_init(pino_vermelho);
     gpio_set_dir(pino_vermelho, GPIO_OUT);
+
+    // O led inicia desligado
+    gpio_put(pino_vermelho, 0);
     
     // Configura o pino do botão como entrada com pull-up interno
     gpio_init(botao_A);
@@ -20,10 +25,16 @@ int main() {
     
     while (true) {
         // Lê o estado do botão (ativo em nível baixo)
-        bool button_pressed = !gpio_get(botao_A); /* Guarda o estado do botão */
+
+        if (gpio_get(botao_A) == 0) {
+            gpio_put(pino_vermelho, 1);
+        }
+
+        else {
+            gpio_put(pino_vermelho, 0);
+        }
         
         // Liga ou desliga o LED conforme o estado do botão
-        gpio_put(pino_vermelho, button_pressed);
         
         sleep_ms(10); // Pequeno atraso para debounce
     }
--- demo_rgb/demo_rgb.c
+++ demo_rgb/demo_rgb.c
@@ -2,30 +2,46 @@
 #include "pico/stdlib.h"
 
 
-/* Acendendo o led */
-// Definição dos pinos do LED RGB na BitDogLab
-#define pino_vermelho 13  // Pino do LED Vermelho
-#define pino_verde 11  // Pino do LED Verde
-
 int main() {
-    // Inicializa o padrão de entrada/saída
+
+    /* Definindo os pinos RGB na BitDogLab*/
+    const uint pino_vermelho = 13;
+    const uint pino_verde = 11;
+    const uint pino_azul = 12;
+
     stdio_init_all();
     
     // Configura os pinos do LED RGB como saída
-    gpio_init(pino_vermelho);
-    gpio_set_dir(pino_vermelho, GPIO_OUT);
     gpio_init(pino_verde);
+    gpio_init(pino_vermelho);
+    gpio_init(pino_azul);
+
+    gpio_set_dir(pino_vermelho, GPIO_OUT);
     gpio_set_dir(pino_verde, GPIO_OUT);
+    gpio_set_dir(pino_azul, GPIO_OUT);
+    /* Por que o led é definido como saída? */
     
+    /* while (true) {
+
+        // Definindo cores do led
+        gpio_put(pino_vermelho, 1);
+        gpio_put(pino_verde, 1);
+        gpio_put(pino_azul, 1);
+
+    } */
+
     while (true) {
-        // Acende o LED vermelho e apaga o verde
+
         gpio_put(pino_vermelho, 1);
         gpio_put(pino_verde, 0);
-        sleep_ms(1000); // Aguarda 1 segundo
+        sleep_ms(3000);
 
-        // Acende o LED verde e apaga o vermelho
-        gpio_put(pino_vermelho, 0);
-        gpio_put(pino_verde, 0);
-        sleep_ms(1000); // Aguarda 1 segundo
+        gpio_put(pino_verde, 1);
+        gpio_put(pino_vermelho, 1);
+        sleep_ms(3000);
+
     }
+
+    return 0;
+
 }
